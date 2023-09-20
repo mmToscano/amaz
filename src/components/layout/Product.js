e81321a63f82738b9f4348feb26c8812a23f9c8d
Matheus Toscano <m.moraes11@outlook.com>
2023-09-20 16:19:55 -0300

    product component adicionado

--- src/components/layout/Product.js
+++ src/components/layout/Product.js
@@ -1,9 +1,18 @@
 import styles from './Product.module.css'
+import defaultImage from '../../img/plants_imgs/carolina_reaper.jpg'
 
 function Product () {
+
+
     return(
         <div className={styles.product}>
-            oi
+            <img src={defaultImage}></img>
+            <h1>Carolina Reaper: 5 sementes</h1>
+            <h2>(Pimenta mais forte do mundo)</h2>
+            <div className={styles.areaDePreco}>
+                <h2><s>R$ 33,98</s></h2>
+                <h2 className={styles.promo}>R$ 28,99</h2>
+            </div>
         </div>
     )
 }
